44e209e15acbda2b841654f40f25fd4088ad055c
ilasaigues <32438118+ilasaigues@users.noreply.github.com>
2019-06-28 11:14:09 -0300

    - Added splash - Added button sounds - Added UI art - Added gameplay BG

---
+++ Assets/Scripts/ButtonClickSound.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class ButtonClickSound : MonoBehaviour
+{
+    public SoundValue clickSound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!clickSound) Destroy(gameObject);
+        else GetComponent<Button>().onClick.AddListener(OnClick);
+    }
+
+    void OnClick()
+    {
+        Director.GetManager<SoundManager>().PlaySound(clickSound);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
